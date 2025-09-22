bb18823e834e4bb1ca40a96d2b5038669cf5afdc
Santiago Gamboa <santiagogamboa.b500@gmail.com>
2025-09-22 17:01:37 -0500

    feat: update icons

--- src/lib/components/icons/AddCircle.svelte
+++ src/lib/components/icons/AddCircle.svelte
@@ -1,1 +1,39 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}><!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ --><g fill="none" stroke="currentColor" stroke-width="1.5"><circle cx="12" cy="12" r="10" opacity=".5"/><path stroke-linecap="round" d="M15 12h-3m0 0H9m3 0V9m0 3v3"/></g></svg>
+<script lang="ts">
+  interface IconProps {
+    size?: string
+    color?: string
+    classes?: string
+  }
+
+  const {
+    size,
+    color,
+    classes
+  }: IconProps = $props()
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size ?? "1em"}
+  height={size ?? "1em"}
+  viewBox="0 0 24 24"
+  class={classes}
+>
+  <!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ -->
+  <g
+    fill="none"
+    stroke={color ?? "currentColor"}
+    stroke-width="1.5"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      r="10"
+      opacity=".5"
+    />
+    <path
+      stroke-linecap="round"
+      d="M15 12h-3m0 0H9m3 0V9m0 3v3"
+    />
+  </g>
+</svg>
--- src/lib/components/icons/ArrowDown.svelte
+++ src/lib/components/icons/ArrowDown.svelte
@@ -1,1 +1,28 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}><!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m19 9l-7 6l-7-6"/></svg>
+<script lang="ts">
+  interface IconProps {
+    size?: string
+    color?: string
+  }
+
+  const {
+    size,
+    color
+  }: IconProps = $props()
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size ?? "1em"}
+  height={size ?? "1em"}
+  viewBox="0 0 24 24"
+>
+  <!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ -->
+   <path
+   fill="none"
+   stroke={color ?? "currentColor"}
+   stroke-linecap="round"
+   stroke-linejoin="round"
+   stroke-width="1.5"
+   d="m19 9l-7 6l-7-6"
+   />
+</svg>
